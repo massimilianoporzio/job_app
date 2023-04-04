f20b7c28da0aa9dd0bd4d22bd7c5187858fddaca
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-04-04 17:50:51 +0200

    prove

--- lib/app/presentation/pages/home_page.dart
+++ lib/app/presentation/pages/home_page.dart
@@ -56,79 +56,82 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 0),
-                  height: 0.08 * mHeight,
-                  // color: Colors.amber,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: TextField(
-                      controller: TextEditingController(),
-                      obscureText: false,
-                      textAlign: TextAlign.start,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                            width: 1,
-                          ),
-                        ),
-                        hintText: "Enter Text",
-                        hintStyle: const TextStyle(
-                          fontWeight: FontWeight.w600,
+            child: Container(
+              child: ListView(
+                // mainAxisSize: MainAxisSize.max,
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 0),
+
+                    // color: Colors.amber,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: TextField(
+                        controller: TextEditingController(),
+                        obscureText: false,
+                        textAlign: TextAlign.start,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
                           fontStyle: FontStyle.normal,
                           fontSize: 14,
+                          color: Theme.of(context).colorScheme.primary,
                         ),
-                        filled: false,
-                        isDense: true,
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 12),
-                        prefixIcon: const Icon(Icons.search, size: 24),
-                        suffixIcon:
-                            const Icon(Icons.filter_alt_outlined, size: 24),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.onPrimary,
+                              width: 1,
+                            ),
+                          ),
+                          hintText: "Ricerca annunci",
+                          hintStyle: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14,
+                          ),
+                          filled: false,
+                          isDense: true,
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 12),
+                          prefixIcon: const Icon(Icons.search, size: 24),
+                          suffixIcon:
+                              const Icon(Icons.filter_alt_outlined, size: 24),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 0.2 * mHeight,
-                  // color: Colors.deepOrange,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          margin: const EdgeInsets.symmetric(vertical: 6),
-                          // height: 0.15 * mHeight,
-                          child: Card(
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            // color: Theme.of(context).colorScheme.primary,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  gradient: LinearGradient(
-                                      colors: [
-                                        Theme.of(context).colorScheme.tertiary,
-                                        Theme.of(context).colorScheme.primary,
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight)),
-                              child: Expanded(
+                  Container(
+                    // height: 0.2 * mHeight,
+                    height: 160,
+                    // color: Colors.deepOrange,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            // margin: const EdgeInsets.symmetric(vertical: 6),
+                            // height: 0.15 * mHeight,
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              // color: Theme.of(context).colorScheme.primary,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Theme.of(context)
+                                              .colorScheme
+                                              .tertiary,
+                                          Theme.of(context).colorScheme.primary,
+                                        ],
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomRight)),
                                 child: Column(
                                   mainAxisSize: MainAxisSize.min,
                                   mainAxisAlignment: MainAxisAlignment.start,
@@ -210,110 +213,109 @@ class HomePage extends StatelessWidget {
                             ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          // color: Colors.yellow,
-                          // height: 0.2 * mHeight,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 0),
-                                height: 0.075 * mHeight,
-                                width: 0.9 * mWidth,
-                                child: Card(
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            // color: Colors.yellow,
+                            // height: 0.2 * mHeight,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 0),
+                                  width: 0.9 * mWidth,
+                                  child: Card(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondaryContainer,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text("9",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelLarge!
+                                                  .copyWith(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 22,
+                                                  )),
+                                          Text(
+                                            "aziende",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelMedium!
+                                                .copyWith(
+                                                    fontSize: 14,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10),
+                                  // height: 0.075 * mHeight,
+                                  width: 0.9 * mWidth,
+                                  child: Card(
                                     color: Theme.of(context)
                                         .colorScheme
-                                        .tertiaryContainer
-                                        .harmonizeWith(Theme.of(context)
-                                            .colorScheme
-                                            .primary),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text("9",
+                                        .onPrimaryContainer
+                                        .withOpacity(0.75),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 4),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text("4",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelLarge!
+                                                  .copyWith(
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .onPrimary,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 22,
+                                                  )),
+                                          Text(
+                                            "annunci\nrecenti",
+                                            textAlign: TextAlign.center,
                                             style: Theme.of(context)
                                                 .textTheme
-                                                .labelLarge!
+                                                .labelMedium!
                                                 .copyWith(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 22,
-                                                )),
-                                        Text(
-                                          "aziende",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .labelMedium!
-                                              .copyWith(
-                                                  fontSize: 14,
-                                                  fontWeight: FontWeight.bold),
-                                        ),
-                                      ],
-                                    )),
-                              ),
-                              Container(
-                                margin:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                // height: 0.075 * mHeight,
-                                width: 0.9 * mWidth,
-                                child: Card(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onPrimaryContainer
-                                      .withOpacity(0.75),
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.symmetric(vertical: 4),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text("4",
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelLarge!
-                                                .copyWith(
-                                                  color: Theme.of(context)
-                                                      .colorScheme
-                                                      .onPrimary,
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 22,
-                                                )),
-                                        Text(
-                                          "annunci\nrecenti",
-                                          textAlign: TextAlign.center,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .labelMedium!
-                                              .copyWith(
-                                                  color: Theme.of(context)
-                                                      .colorScheme
-                                                      .onPrimary,
-                                                  fontSize: 12,
-                                                  fontWeight: FontWeight.bold),
-                                        ),
-                                      ],
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .onPrimary,
+                                                    fontSize: 12,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                    child: Container(
-                        // color: Colors.deepPurple,
-                        )),
-              ],
+                  Expanded(
+                      child: Container(
+                    color: Colors.deepPurple,
+                    height: 500,
+                  )),
+                ],
+              ),
             )),
       ),
     );
